89e54d40f6883e82d04991759cb2f424389c011f
unknown <nduflot@dawan.fr>
2022-11-18 10:07:29 +0100

    javascript pt-6 ajax avec fetch affichage et recherche

--- js/functions.js
+++ js/functions.js
@@ -1,4 +1,59 @@
 function jsonToTable(data, search = ''){
+    search = search.toLowerCase();
+    let premierParcour = true;
+    let thead = '<tr>';
+    let tempLine = '';
+    let tbody = '';
+    let searchOK = ('' === search)?true:false;
+    let compare = '';
 
-    return ['haha ', 'hoho']
+    data.forEach(element => {
+        tempLine = tempLine + '<tr>';
+        for(key in element){
+            if(premierParcour){
+                thead = thead + `<th>${key}</th>`;
+            }
+            if('object' !== typeof element[key]){
+                compare = element[key].toString().toLowerCase();
+                if(compare.indexOf(search) >= 0){
+                    searchOK = true;
+                }
+                tempLine = tempLine + '<td>';
+                tempLine = tempLine + ((compare.indexOf(search) >= 0 && '' !== search)?'<mark>': '');
+                tempLine = tempLine + element[key];
+                tempLine = tempLine + ((compare.indexOf(search) >= 0 && '' !== search)?'</mark>': '');
+                tempLine = tempLine + '</td>';
+            }else{
+                tempLine = tempLine + '<td>';
+                
+                for(item in element[key]){
+                    if('object' !== typeof element[key][item]){
+                        compare = element[key][item].toString().toLowerCase();
+                        if(compare.indexOf(search) >= 0){
+                            searchOK = true;
+                        }
+                        tempLine = tempLine + `<b>${item}</b>`;
+                        tempLine = tempLine + ((compare.indexOf(search) >= 0 && '' !== search)?'<mark>': '');
+                        tempLine = tempLine + `<i>${element[key][item]}</i><br />`;
+                        tempLine = tempLine + ((compare.indexOf(search) >= 0 && '' !== search)?'</mark>': '');
+                    }
+                }
+                
+                tempLine = tempLine + '</td>';
+            }
+        }
+        
+        tempLine = tempLine + '</tr>';
+
+        /* vérification de la ligne valide à ajouter au tbody */
+        if(searchOK){
+            tbody = tbody + tempLine;
+        }
+        premierParcour = false;
+        searchOK = ('' === search)?true:false;
+        tempLine = '';
+    });
+
+    thead = thead + '</tr>';
+    return [thead, tbody]
 }
